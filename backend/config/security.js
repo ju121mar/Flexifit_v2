c0968d4f99c69744baca5e49d9ae798ed3b555cf
Julia + Martin <ju121mar@htwg-konstanz.de>
2025-01-09 16:01:01 +0100

    backend Konfigurationen

--- backend/config/security.js
+++ backend/config/security.js
@@ -31,7 +31,7 @@ module.exports.security = {
 
   cors: {
     allRoutes: true,
-    allowOrigins: ['http://localhost:5173','http://localhost:5174'],
+    allowOrigins: ['http://localhost:5173','http://localhost:5174', 'https://flexifit.onrender.com'],
     allowCredentials: true,
   },
 
